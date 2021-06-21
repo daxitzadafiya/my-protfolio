17e3af5ccf4ed9d805dd78ec205d3566d50f2db8
daxit zadafiya <daxitzadafiya91@gmail.com>
2021-06-22 02:11:01 +0530

    testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,7 @@ modalCloses.forEach((modalClose) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperProtfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop:true,
     navigation: {
@@ -112,7 +112,21 @@ let swiper = new Swiper(".portfolio__container", {
   });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween:48,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets:true,
+    },
+    breakpoints: {
+        568: {
+            slidsPerview:2
+        }
+    }
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
