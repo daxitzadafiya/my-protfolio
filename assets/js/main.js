86100ca280d688c457ae5cf993a33bbb9b93cf76
Kishan <sm.kishan.kanejiya@gmail.com>
2021-07-11 23:30:03 +0530

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,3 +193,5 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+console.log(window.screen.width);
